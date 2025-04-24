21a87bdf62220a3b324bba69501310252c3554c2
ddavevo <ddavepersona@gmail.com>
2025-04-24 11:27:16 -0700

    Attempt 2

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,16 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  const nextConfig = {
     output: 'export',
     images: {
       unoptimized: true,
     },
     basePath: '/cogs125-react-dv', // match your repo name!
     assetPrefix: '/nextjs-dashboard/',
-  };
-  
-  module.exports = nextConfig;
 };
 
+module.exports = nextConfig;
 export default nextConfig;
