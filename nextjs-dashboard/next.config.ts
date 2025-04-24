2fac7b93cdb45cdc1823ea63641af8047e6fd878
ddavevo <ddavepersona@gmail.com>
2025-04-24 11:25:14 -0700

    Adding stuff for GitHub pages

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/cogs125-react-dv', // match your repo name!
+    assetPrefix: '/nextjs-dashboard/',
+  };
+  
+  module.exports = nextConfig;
 };
 
 export default nextConfig;
