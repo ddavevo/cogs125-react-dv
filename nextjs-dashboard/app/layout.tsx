466932c06e67f6981c73bd1b52b2f39db3ff5978
ddavevo <ddavepersona@gmail.com>
2025-04-29 13:31:14 -0700

    ???????????????

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,12 +1,16 @@
 import './ui/global.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata = {
+  title: 'My App',
+  description: 'This is my app.',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <base href="/cogs125-react-dv/" />
+      </head>
       <body>{children}</body>
     </html>
   );
