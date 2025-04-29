1b1a6d147df975dbae977c5643dd07939da86c54
ddavevo <ddavepersona@gmail.com>
2025-04-29 10:38:04 -0700

    trying a relative link

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
-import { Button } from '@/app/ui/button';
+import { Button } from '../../ui/button';
 export const dynamic = 'force-static';
 
 export default function Page() {
