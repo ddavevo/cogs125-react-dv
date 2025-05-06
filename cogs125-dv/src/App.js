a1d0db55108d9fe8481e1751960c31f17e56fe07
ddavevo <ddavepersona@gmail.com>
2025-05-06 01:53:54 -0700

    trying hashes

--- cogs125-dv/src/App.js
+++ cogs125-dv/src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
