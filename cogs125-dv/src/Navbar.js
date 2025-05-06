520637942944f2546707e4b1eb76367c6f0d9e6f
ddavevo <ddavepersona@gmail.com>
2025-05-06 02:01:38 -0700

    reverting back?

--- cogs125-dv/src/Navbar.js
+++ cogs125-dv/src/Navbar.js
@@ -3,9 +3,9 @@ const Navbar = () => {
       <nav className="navbar">
         <h1>COGS125 DV</h1>
         <div className="headers">
-            <a href="/cogs125-react-dv">Home</a>
-            <a href="/cogs125-react-dv/about">About</a>
-            <a href="/cogs125-react-dv/contact">Contact</a>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
         </div>
       </nav>
     )
